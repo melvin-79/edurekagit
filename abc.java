6e517188ec1daa16ffdc7df14e6d591d5a433297
Melvin.Edureka <Melvin@edureka>
2024-05-16 15:12:49 +0000

    Adding a line to abc.java and creating a new file called bcd.java

--- abc.java
+++ abc.java
@@ -3,3 +3,4 @@ Second line and more information
 third line
 fourth line
 for testing git commit -a success scenario
+for testing git commit -a failure scenario as i have created a new file called bcd.java
